1aceb01e2139c30f1e557a7971c49ba757706172
Vikas Panchal <eng.vikas90@gmail.com>
2024-02-25 05:13:29 +0530

    hjkj
    bhuljh

--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -1,21 +1,34 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "../../css/MoviePage/movie.css"
 import { CircularProgressbar,buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Autoplay,Pagination } from 'swiper/modules';
+import ReactPlayer from 'react-player/lazy'
 
 const banner1image = "https://www.tallengestore.com/cdn/shop/products/Dora_The_Explorer_And_The_Lost_City_Of_Gold_-_Hollywood_English_Movie_Poster_1_3fd98041-803c-4491-9d4a-a0a1d5533aae.jpg?v=1577693642"
 
 function page() {
-
   const value = 7.9;
+  const [hasWindow, setHasWindow] = useState(false);
+  console.log("typeof window" ,typeof window );
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setHasWindow(true);
+    }
+  }, []);
 
   return (
     <div className='moviePageHeadContainer' style={{backgroundImage:`url(${"https://image.tmdb.org/t/p/original/nTPFkLUARmo1bYHfkfdNpRKgEOs.jpg"})`,backgroundSize:'contain'}}>
       <div className='vghui'>
-    <div className='headContainer'>
-      <div className='bannerImageHeadContainer' >
+        <div className='headContainer'>
+
+          <div className='bannerImageHeadContainer' >
 
           <div className='bannerDiv' style={{maxWidth:350}}>
             <img src={banner1image} />
@@ -63,35 +76,363 @@ function page() {
           </div>
 
 
-      </div>
+          </div>
 
-
-
-          <div>
-            <div>
-              <p>Top Cast</p>
+          {/* Top cast */}
+          <div style={{padding:0,display:'flex',flexDirection:'column',width:"100%"}}>
+            <div style={{padding:0}}>
+              <p style={{padding:0,textAlign:'left'}}>Top Cast</p>
             </div>
-            <div>
+            <div style={{padding:0,width:'100%'}}>
+              <div style={{display:'flex',flexWrap:'wrap', flexDirection:'row',justifyContent:'flex-start',gap:40,alignItems:'center',width:'100%'}}>
+
+                <div style={{padding:0,width:150,height:200}}>
+                  <img src='https://image.tmdb.org/t/p/original/yO356gXWSr6fgMw4UH1eYuhdOHI.jpg' style={{width:150,height:150,objectFit:'cover',borderRadius:12}}/>
+                  <p>Sydney Sweeney</p>
+                  <p>Bea</p>
+                </div>
+
+                <div style={{padding:0,width:150,height:200}}>
+                  <img src='https://image.tmdb.org/t/p/original/yO356gXWSr6fgMw4UH1eYuhdOHI.jpg' style={{width:150,height:150,objectFit:'cover',borderRadius:12}}/>
+                  <p>Sydney Sweeney</p>
+                  <p>Bea</p>
+                </div>
+
+                <div style={{padding:0,width:150,height:200}}>
+                  <img src='https://image.tmdb.org/t/p/original/yO356gXWSr6fgMw4UH1eYuhdOHI.jpg' style={{width:150,height:150,objectFit:'cover',borderRadius:12}}/>
+                  <p>Sydney Sweeney</p>
+                  <p>Bea</p>
+                </div>
+
+                <div style={{padding:0,width:150,height:200}}>
+                  <img src='https://image.tmdb.org/t/p/original/yO356gXWSr6fgMw4UH1eYuhdOHI.jpg' style={{width:150,height:150,objectFit:'cover',borderRadius:12}}/>
+                  <p>Sydney Sweeney</p>
+                  <p>Bea</p>
+                </div>
+
+                <div style={{padding:0,width:150,height:200}}>
+                  <img src='https://image.tmdb.org/t/p/original/yO356gXWSr6fgMw4UH1eYuhdOHI.jpg' style={{width:150,height:150,objectFit:'cover',borderRadius:12}}/>
+                  <p>Sydney Sweeney</p>
+                  <p>Bea</p>
+                </div>
+
+                <div style={{padding:0,width:150,height:200}}>
+                  <img src='https://image.tmdb.org/t/p/original/yO356gXWSr6fgMw4UH1eYuhdOHI.jpg' style={{width:150,height:150,objectFit:'cover',borderRadius:12}}/>
+                  <p>Sydney Sweeney</p>
+                  <p>Bea</p>
+                </div>
+                
+              </div>
               
 
             </div>
+          </div>
+
+          {/* Official Videos */}
+          <div style={{padding:0,display:'flex',flexDirection:'column',width:"100%"}}>
+            <div style={{padding:0,textAlign:'left'}}>
+              <span style={{padding:0,textAlign:'left'}}>Official Videos</span>
+            </div>
+            <div style={{padding:0,display:'flex',flexDirection:'row',justifyContent:'flex-start',alignItems:'center',width:"100%",gap:40}}>
+                <div style={{width:"100%",height:"auto"}}>
+                      {
+                        hasWindow && <Swiper
+                        slidesPerView={'auto'}
+                        spaceBetween={30}
+                        // loop={true}
+                        // autoplay={{
+                        //   delay: 3000,
+                        //   disableOnInteraction: false,
+                        // }}
+                        pagination={{
+                          clickable: true,
+                        }}
+                        modules={[Autoplay,Pagination]}
+                        className="mySwiper"
+                        style={{marginTop:1}}
+                      >
+                        <SwiperSlide style={{width:300,height:200}} >
+                          <ReactPlayer className="playerz" url='https://www.youtube.com/watch?v=LXb3EKWsInQ' width='100%' height='100%' controls />
+                        </SwiperSlide>
+                        <SwiperSlide style={{width:300,height:200}}>
+                          <ReactPlayer className="playerz" url='https://www.youtube.com/watch?v=LXb3EKWsInQ' width='100%' height='100%'/>
+                        </SwiperSlide>
+                        <SwiperSlide style={{width:300,height:200}}>
+                          <ReactPlayer className="playerz" url='https://www.youtube.com/watch?v=LXb3EKWsInQ' width='100%' height='100%'/>
+                        </SwiperSlide>
+                        <SwiperSlide style={{width:300,height:200}}>
+                          <ReactPlayer className="playerz" url='https://www.youtube.com/watch?v=LXb3EKWsInQ' width='100%' height='100%'/>
+                        </SwiperSlide>
+                        <SwiperSlide style={{width:300,height:200}}>
+                          <ReactPlayer className="playerz" url='https://www.youtube.com/watch?v=LXb3EKWsInQ' width='100%' height='100%'/>
+                        </SwiperSlide>
+                        <SwiperSlide style={{width:300,height:200}}>
+                          <ReactPlayer className="playerz" url='https://www.youtube.com/watch?v=LXb3EKWsInQ' width='100%' height='100%'/>
+                        </SwiperSlide>
+                        <SwiperSlide style={{width:300,height:200}}>
+                          <ReactPlayer className="playerz" url='https://www.youtube.com/watch?v=LXb3EKWsInQ' width='100%' height='100%'/>
+                        </SwiperSlide>
+                        
+                          </Swiper>
+                        
+                        
+                        
+                        
+                        
+                        // <div style={{padding:0,display:'flex',flexDirection:'row', gap:20,width:'100%', height:'auto'}}>
+                        //   <ReactPlayer className="playerz" url='https://www.youtube.com/watch?v=LXb3EKWsInQ' width='300px' height='200px'/>
+
+                        //   <ReactPlayer className="playerz" url='https://www.youtube.com/watch?v=LXb3EKWsInQ' width='300px' height='200px'/>
+
+                        //   <ReactPlayer className="playerz" url='https://www.youtube.com/watch?v=LXb3EKWsInQ' width='300px' height='200px'/>  
+                        
+                        
+                        // </div>
+                        }
+                </div>  
+                
+            </div>
+            <div>
+              
+            </div>
+          </div>
+
+          {/* Similar video */}
+          <div style={{width:"100%",padding:0}}>
+          <div style={{padding:0}}>
+              <p style={{padding:0,textAlign:'left'}}>Similar Movies</p>
+            </div>
+            
+          <Swiper
+        slidesPerView={'auto'}
+        spaceBetween={30}
+        // loop={true}
+        // autoplay={{
+        //   delay: 3000,
+        //   disableOnInteraction: false,
+        // }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay,Pagination]}
+        className="mySwiper"
+        style={{marginTop:1}}
+      >
+        <SwiperSlide >
+          
+          <img src={banner1image}/>
+          <div className='carousalPercentagediv'>
+          <CircularProgressbar value={value} maxValue={10} text={`${value}`} styles={buildStyles({textSize: '35px',textColor: '#000',trailColor: '#fff',pathColor: `${value < 7 ? "orange": "green" }`})} />
+          </div>
+          <div className='generes'>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>Drama</span>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>History</span>
+          </div>
+          <div className='carousalDescription'>
+            <span className='carousalDescriptionSpan1'>The Zone of Interest</span>
+            <span className='carousalDescriptionSpan2'>Dec 15, 2023</span>
           </div>
           
+          </SwiperSlide>
 
-          <div>
-            <div>
-              <p>Official Videos</p>
-            </div>
-            <div>
-              
-            </div>
+          <SwiperSlide >
+          
+          <img src={banner1image}/>
+          <div className='carousalPercentagediv'>
+          <CircularProgressbar value={value} maxValue={10} text={`${value}`} styles={buildStyles({textSize: '35px',textColor: '#000',trailColor: '#fff',pathColor: `${value < 7 ? "orange": "green" }`})} />
+          </div>
+          <div className='generes'>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>Drama</span>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>History</span>
+          </div>
+          <div className='carousalDescription'>
+            <span className='carousalDescriptionSpan1'>The Zone of Interest</span>
+            <span className='carousalDescriptionSpan2'>Dec 15, 2023</span>
+          </div>
+          
+          </SwiperSlide>
+
+          <SwiperSlide >
+          
+          <img src={banner1image}/>
+          <div className='carousalPercentagediv'>
+          <CircularProgressbar value={value} maxValue={10} text={`${value}`} styles={buildStyles({textSize: '35px',textColor: '#000',trailColor: '#fff',pathColor: `${value < 7 ? "orange": "green" }`})} />
+          </div>
+          <div className='generes'>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>Drama</span>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>History</span>
+          </div>
+          <div className='carousalDescription'>
+            <span className='carousalDescriptionSpan1'>The Zone of Interest</span>
+            <span className='carousalDescriptionSpan2'>Dec 15, 2023</span>
+          </div>
+          
+          </SwiperSlide>
+
+          <SwiperSlide >
+          
+          <img src={banner1image}/>
+          <div className='carousalPercentagediv'>
+          <CircularProgressbar value={value} maxValue={10} text={`${value}`} styles={buildStyles({textSize: '35px',textColor: '#000',trailColor: '#fff',pathColor: `${value < 7 ? "orange": "green" }`})} />
+          </div>
+          <div className='generes'>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>Drama</span>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>History</span>
+          </div>
+          <div className='carousalDescription'>
+            <span className='carousalDescriptionSpan1'>The Zone of Interest</span>
+            <span className='carousalDescriptionSpan2'>Dec 15, 2023</span>
+          </div>
+          
+          </SwiperSlide>
+
+          <SwiperSlide >
+          
+          <img src={banner1image}/>
+          <div className='carousalPercentagediv'>
+          <CircularProgressbar value={value} maxValue={10} text={`${value}`} styles={buildStyles({textSize: '35px',textColor: '#000',trailColor: '#fff',pathColor: `${value < 7 ? "orange": "green" }`})} />
+          </div>
+          <div className='generes'>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>Drama</span>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>History</span>
+          </div>
+          <div className='carousalDescription'>
+            <span className='carousalDescriptionSpan1'>The Zone of Interest</span>
+            <span className='carousalDescriptionSpan2'>Dec 15, 2023</span>
+          </div>
+          
+          </SwiperSlide>
+
+          <SwiperSlide >
+          
+          <img src={banner1image}/>
+          <div className='carousalPercentagediv'>
+          <CircularProgressbar value={value} maxValue={10} text={`${value}`} styles={buildStyles({textSize: '35px',textColor: '#000',trailColor: '#fff',pathColor: `${value < 7 ? "orange": "green" }`})} />
+          </div>
+          <div className='generes'>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>Drama</span>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>History</span>
+          </div>
+          <div className='carousalDescription'>
+            <span className='carousalDescriptionSpan1'>The Zone of Interest</span>
+            <span className='carousalDescriptionSpan2'>Dec 15, 2023</span>
+          </div>
+          
+          </SwiperSlide>
+
+          <SwiperSlide >
+          
+          <img src={banner1image}/>
+          <div className='carousalPercentagediv'>
+          <CircularProgressbar value={value} maxValue={10} text={`${value}`} styles={buildStyles({textSize: '35px',textColor: '#000',trailColor: '#fff',pathColor: `${value < 7 ? "orange": "green" }`})} />
+          </div>
+          <div className='generes'>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>Drama</span>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>History</span>
+          </div>
+          <div className='carousalDescription'>
+            <span className='carousalDescriptionSpan1'>The Zone of Interest</span>
+            <span className='carousalDescriptionSpan2'>Dec 15, 2023</span>
+          </div>
+          
+          </SwiperSlide>
+
+          <SwiperSlide >
+          
+          <img src={banner1image}/>
+          <div className='carousalPercentagediv'>
+          <CircularProgressbar value={value} maxValue={10} text={`${value}`} styles={buildStyles({textSize: '35px',textColor: '#000',trailColor: '#fff',pathColor: `${value < 7 ? "orange": "green" }`})} />
+          </div>
+          <div className='generes'>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>Drama</span>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>History</span>
+          </div>
+          <div className='carousalDescription'>
+            <span className='carousalDescriptionSpan1'>The Zone of Interest</span>
+            <span className='carousalDescriptionSpan2'>Dec 15, 2023</span>
+          </div>
+          
+          </SwiperSlide>
+
+          <SwiperSlide >
+          
+          <img src={banner1image}/>
+          <div className='carousalPercentagediv'>
+          <CircularProgressbar value={value} maxValue={10} text={`${value}`} styles={buildStyles({textSize: '35px',textColor: '#000',trailColor: '#fff',pathColor: `${value < 7 ? "orange": "green" }`})} />
+          </div>
+          <div className='generes'>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>Drama</span>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>History</span>
+          </div>
+          <div className='carousalDescription'>
+            <span className='carousalDescriptionSpan1'>The Zone of Interest</span>
+            <span className='carousalDescriptionSpan2'>Dec 15, 2023</span>
+          </div>
+          
+          </SwiperSlide>
+
+          <SwiperSlide >
+          
+          <img src={banner1image}/>
+          <div className='carousalPercentagediv'>
+          <CircularProgressbar value={value} maxValue={10} text={`${value}`} styles={buildStyles({textSize: '35px',textColor: '#000',trailColor: '#fff',pathColor: `${value < 7 ? "orange": "green" }`})} />
+          </div>
+          <div className='generes'>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>Drama</span>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>History</span>
+          </div>
+          <div className='carousalDescription'>
+            <span className='carousalDescriptionSpan1'>The Zone of Interest</span>
+            <span className='carousalDescriptionSpan2'>Dec 15, 2023</span>
+          </div>
+          
+          </SwiperSlide>
+
+          <SwiperSlide >
+          
+          <img src={banner1image}/>
+          <div className='carousalPercentagediv'>
+          <CircularProgressbar value={value} maxValue={10} text={`${value}`} styles={buildStyles({textSize: '35px',textColor: '#000',trailColor: '#fff',pathColor: `${value < 7 ? "orange": "green" }`})} />
+          </div>
+          <div className='generes'>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>Drama</span>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>History</span>
+          </div>
+          <div className='carousalDescription'>
+            <span className='carousalDescriptionSpan1'>The Zone of Interest</span>
+            <span className='carousalDescriptionSpan2'>Dec 15, 2023</span>
+          </div>
+          
+          </SwiperSlide>
+
+          <SwiperSlide >
+          
+          <img src={banner1image}/>
+          <div className='carousalPercentagediv'>
+          <CircularProgressbar value={value} maxValue={10} text={`${value}`} styles={buildStyles({textSize: '35px',textColor: '#000',trailColor: '#fff',pathColor: `${value < 7 ? "orange": "green" }`})} />
+          </div>
+          <div className='generes'>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>Drama</span>
+            <span style={{backgroundColor:'#da2f68',padding:"0px 5px",borderRadius:10,fontSize:12}}>History</span>
+          </div>
+          <div className='carousalDescription'>
+            <span className='carousalDescriptionSpan1'>The Zone of Interest</span>
+            <span className='carousalDescriptionSpan2'>Dec 15, 2023</span>
+          </div>
+          
+          </SwiperSlide>
+        
+          
+        
+      </Swiper>
+
+
+
           </div>
 
-          <div>
-            similir videos
-          </div>
+
         </div>
-        </div>
+      </div>
     </div>
   )
 }
